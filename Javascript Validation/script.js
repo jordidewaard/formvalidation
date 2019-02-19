10b4ddbd95990d33969e9275536db6e724ee7093
LAPTOP-4A79OJ3C\jordi <jordidewaard97@gmail.com>
2019-02-19 14:35:25 +0100

    HTML Form afgemaakt en Javascript Form halverwege

---
+++ Javascript Validation/script.js
@@ -0,0 +1,13 @@
+function validateForm() {
+    var fn = document.forms["jsvalidation"]["firstname"].value;
+    if (fn == "") {
+        alert("Voornaam niet goed");
+        return false;
+    }
+    var ln = document.forms["jsvalidation"]["lastname"].value;
+    if (ln == "") {
+        alert("Achternaam niet goed");
+        return false;
+    }
+
+}
