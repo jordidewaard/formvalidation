eaf053480f71a84f91d4998fd2ad26a9b614f3a0
LAPTOP-4A79OJ3C\jordi <jordidewaard97@gmail.com>
2019-02-22 00:43:51 +0100

    PHP Validatie aangemaakt, Dingen uitgeprobeerd in JS Validatie en kleine verandering in HTML Validatie

--- Javascript Validation/script.js
+++ Javascript Validation/script.js
@@ -1,9 +1,15 @@
+var square = document.getElementById("square");
+
 function validateForm() {
     var fn = document.forms["jsvalidation"]["firstname"].value;
     if (fn == "") {
+        square.style.backgroundColor = "#fa4";
         alert("Voornaam niet goed");
         return false;
+    } else {
+        square.style.backgroundColor = "transparent";
     }
+
     var ln = document.forms["jsvalidation"]["lastname"].value;
     if (ln == "") {
         alert("Achternaam niet goed");
---
+++ PHP Validation/form.php
@@ -0,0 +1,66 @@
+<?php
+function filterName($field){
+    $field = filter_var(trim($field), FILTER_SANITIZE_STRING);
+
+    if(filter_var($field, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        return $field;
+    } else{
+        return FALSE;
+    }
+}
+function filterEmail($field){
+    $field = filter_var(trim($field), FILTER_SANITIZE_EMAIL);
+
+    if(filter_var($field, FILTER_VALIDATE_EMAIL)){
+        return $field;
+    } else{
+        return FALSE;
+    }
+}
+
+$nameErr = $emailErr = "";
+$name = $email = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if (empty($_POST["name"])) {
+        $nameErr = "Voer je naam in";
+    } else {
+        $name = filterName($_POST["name"]);
+        if ($name == FALSE) {
+            $nameErr = "Voer een geldige naam in";
+        }
+    }
+
+    if (empty($_POST["email"])) {
+        $emailErr = "Voer je e-mail adres in";
+    } else {
+        $email = filterEmail($_POST["email"]);
+        if ($email == FALSE) {
+            $emailErr = "Voer een geldig e-mail adres in";
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <title>PHP Validatie Form</title>
+</head>
+<body>
+<form action="form.php" method="post" class="w3-container">
+        <label for="inputName">Roepnaam:</label><br>
+        <input type="text" name="name" id="inputName" value="<?php echo $name; ?>">
+        <span class="error"><?php echo $nameErr; ?></span>
+        <br>
+        <label for="inputEmail">Email:</label><br>
+        <input type="text" name="email" id="inputEmail" value="<?php echo $email; ?>">
+        <span class="error"><?php echo $emailErr; ?></span>
+        <br>
+    <input type="submit" value="Opslaan">
+</form>
+</body>
+</html>
