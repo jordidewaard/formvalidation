3d3305d2f46f974d43e21e46a80d8521fcdc2f75
LAPTOP-4A79OJ3C\jordi <jordidewaard97@gmail.com>
2019-02-22 12:29:42 +0100

    Einde dag vrijdag

--- Javascript Validation/script.js
+++ Javascript Validation/script.js
@@ -2,13 +2,19 @@ function validateForm() {
     var fn = document.forms["jsvalidation"]["firstname"].value;
     if (fn == "") {
         alert("Voornaam niet goed");
+        document.forms["jsvalidation"]["firstname"].style.border = '1px solid red';
         return false;
+    } else {
+        document.forms["jsvalidation"]["firstname"].style.border = '1px solid gray';
     }
 
     var ln = document.forms["jsvalidation"]["lastname"].value;
     if (ln == "") {
         alert("Achternaam niet goed");
+        document.forms["jsvalidation"]["lastname"].style.border = '1px solid red';
         return false;
+
+    } else {
+        document.forms["jsvalidation"]["lastname"].style.border = '1px solid gray';
     }
-    console.log(ln);
 }
